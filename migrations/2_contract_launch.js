679903fe25b701e0f172adfb46a1d5e06f7f9db9
hollandcodeandgraphics <hollandcodeandgraphics@gmail.com>
2020-04-13 15:33:13 -0700

    updated bridge to match proper sig pattern

--- migrations/2_contract_launch.js
+++ migrations/2_contract_launch.js
@@ -4,10 +4,15 @@ const VUSD_TEST = artifacts.require("VUSD_TEST");
 
 
 ///https://ethereum.stackexchange.com/questions/17551/how-to-upgrade-solidity-compiler-in-truffle
-module.exports = function(deployer) {
-    deployer.deploy(MultisigControl);
-    deployer.deploy(Vega_Bridge_ERC20);
-    deployer.deploy(VUSD_TEST, "VUSD_TEST", "VUSD", 18, 1000000);
+module.exports = async function(deployer) {
+    await deployer.deploy(MultisigControl);
+    await deployer.deploy(Vega_Bridge_ERC20);
+    await deployer.deploy(VUSD_TEST, "VUSD_TEST", "VUSD", 18, 1000000);
+    
+    //TODO: remove this to vote it in properly
+    //NOTE this will break the test
+    let vega_bridge_erc20_instance = await Vega_Bridge_ERC20.deployed();
+    await vega_bridge_erc20_instance.whitelist_asset_admin(VUSD_TEST.address, 0);
 
 };
 
