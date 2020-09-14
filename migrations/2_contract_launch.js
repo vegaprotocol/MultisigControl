0a67de0f49c33614306edee66a03fc122bb9af26
C0deMunk33 <hollandcodeandgraphics@gmail.com>
2020-09-14 12:20:53 -0700

    Created test to try deposit/withdrawal round trip.

--- migrations/2_contract_launch.js
+++ migrations/2_contract_launch.js
@@ -1,5 +1,6 @@
 const MultisigControl = artifacts.require("MultisigControl");
 const Vega_Bridge_ERC20 = artifacts.require("Vega_Bridge_ERC20");
+const Vega_Bridge_ETH = artifacts.require("Vega_Bridge_ETH");
 const VUSD_TEST = artifacts.require("VUSD_TEST");
 
 
@@ -7,12 +8,15 @@ const VUSD_TEST = artifacts.require("VUSD_TEST");
 module.exports = async function(deployer) {
     await deployer.deploy(MultisigControl);
     await deployer.deploy(Vega_Bridge_ERC20);
+    await deployer.deploy(Vega_Bridge_ETH, "0x00");
    // await deployer.deploy(VUSD_TEST, "VUSD_TEST", "VUSD", 18, 1000000);
     
     //TODO: remove this to vote it in properly
     //NOTE this will break the test
     let vega_bridge_erc20_instance = await Vega_Bridge_ERC20.deployed();
+    let vega_bridge_eth_instance = await Vega_Bridge_ETH.deployed();
     await vega_bridge_erc20_instance.set_multisig_control(MultisigControl.address);
+    await vega_bridge_eth_instance.set_multisig_control(MultisigControl.address);
  //   await vega_bridge_erc20_instance.whitelist_asset_admin(VUSD_TEST.address, 0);
 
 };
