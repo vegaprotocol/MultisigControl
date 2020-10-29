c751c6f99ca8a295b84201fd8bcba083ed5d7063
domvega <dom@vega.xyz>
2020-10-29 08:47:44 +0000

    #20 un-commented VUSD5 lines on 2_contract_launch.js

--- migrations/2_contract_launch.js
+++ migrations/2_contract_launch.js
@@ -7,13 +7,13 @@ const VUSD_TEST = artifacts.require("VUSD_TEST");
 module.exports = async function(deployer) {
     await deployer.deploy(MultisigControl);
     await deployer.deploy(Vega_Bridge_ERC20);
-   // await deployer.deploy(VUSD_TEST, "VUSD_TEST", "VUSD", 18, 1000000);
+    await deployer.deploy(VUSD_TEST, "VUSD_TEST", "VUSD", 18, 1000000);
     
     //TODO: remove this to vote it in properly
     //NOTE this will break the test
     let vega_bridge_erc20_instance = await Vega_Bridge_ERC20.deployed();
     await vega_bridge_erc20_instance.set_multisig_control(MultisigControl.address);
- //   await vega_bridge_erc20_instance.whitelist_asset_admin(VUSD_TEST.address, 0);
+    await vega_bridge_erc20_instance.whitelist_asset_admin(VUSD_TEST.address, 0);
 
 };
 
