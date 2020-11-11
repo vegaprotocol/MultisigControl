d0e2ddbbe21cb52f971a112306f07ccc7fa46967
C0deMunk33 <hollandcodeandgraphics@gmail.com>
2020-11-11 15:29:52 -0800

    Created example_scripts/truffle_logic_switch_script_ganache.js
    The logic switching function appears to work, next up to try the multisig admin change.

--- migrations/2_contract_launch.js
+++ migrations/2_contract_launch.js
@@ -9,7 +9,11 @@ const VUSD5_TEST = artifacts.require("VUSD5_TEST");
 module.exports = async function(deployer) {
     await deployer.deploy(MultisigControl);
     await deployer.deploy(ERC20_Asset_Pool, MultisigControl.address);
-    await deployer.deploy(ERC20_Bridge_Logic, ERC20_Asset_Pool.address, MultisigControl.address);
+    let logic_1 = await deployer.deploy(ERC20_Bridge_Logic, ERC20_Asset_Pool.address, MultisigControl.address);
+    let logic_2 = await deployer.deploy(ERC20_Bridge_Logic, ERC20_Asset_Pool.address, MultisigControl.address);
+
+    console.log(logic_1.address);
+    console.log(logic_2.address);
 
     await deployer.deploy(VUSD5_TEST, "VUSD5_TEST", "VUSD5", 5, 1000000);
     
@@ -18,8 +22,11 @@ module.exports = async function(deployer) {
     let erc20_asset_pool_instance = await ERC20_Asset_Pool.deployed();
     let erc20_bridge_logic_instance = await ERC20_Bridge_Logic.deployed();
 
-    await erc20_asset_pool_instance.set_bridge_address_admin(ERC20_Bridge_Logic.address);
-    await erc20_bridge_logic_instance.list_asset_admin(VUSD5_TEST.address, 0, "0x460be4264be2d8e3d7a85696ec66d5c5a86e19617f7dc4ddfe127e30b3bfd620");
 
+    await erc20_asset_pool_instance.set_bridge_address_admin(logic_1.address);
+
+    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    console.log(logic_1.address)
+    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 };
 
