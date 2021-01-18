cc480a1e58845b91b1903f109cf64051a9d25597
danny <hollandcodeandgraphics@gmail.com>
2021-01-18 09:42:56 -0800

    deployed new dev ropsten

--- ropsten_deploy_details/dev/ERC20_Asset_Pool.sol
+++ ropsten_deploy_details/dev/ERC20_Asset_Pool.sol
@@ -1,64 +1,69 @@
-pragma solidity ^0.5.16;
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.7.6;
 
 import "./IMultisigControl.sol";
 import "./IERC20.sol";
 
-//TODO remove before mainnet
-import "./Ownable.sol";
-import "./Killable.sol";
-
-contract ERC20_Asset_Pool is Ownable, Killable  /*TODO Remove Owner and Killable before Mainnet*/{
+contract ERC20_Asset_Pool {
+    event Multisig_Control_Set(address indexed new_address);
+    event Bridge_Address_Set(address indexed new_address);
 
     address public multisig_control_address;
     address public erc20_bridge_address;
 
-    /////////////////TODO: Remove before flight
-    function set_bridge_address_admin(address new_address) public onlyOwner{
-        erc20_bridge_address = new_address;
-    }
-    ////////////////////TODO end remove before flight
-
-    constructor(address multisig_control) public{
+    constructor(address multisig_control) {
         multisig_control_address = multisig_control;
+        emit Multisig_Control_Set(multisig_control);
     }
+
     function set_multisig_control(address new_address, uint256 nonce, bytes memory signatures) public {
         bytes memory message = abi.encode(new_address, nonce, 'set_multisig_control');
-        require(IMultisigControl(multisig_control_address).verify_signatures(signatures, message, nonce));
+        require(IMultisigControl(multisig_control_address).verify_signatures(signatures, message, nonce), "bad signatures");
         multisig_control_address = new_address;
+        emit Multisig_Control_Set(new_address);
     }
 
     function set_bridge_address(address new_address, uint256 nonce, bytes memory signatures) public {
         bytes memory message = abi.encode(new_address, nonce, 'set_bridge_address');
-        require(IMultisigControl(multisig_control_address).verify_signatures(signatures, message, nonce));
+        require(IMultisigControl(multisig_control_address).verify_signatures(signatures, message, nonce), "bad signatures");
         erc20_bridge_address = new_address;
+        emit Bridge_Address_Set(new_address);
     }
 
-    //NOTE: To deposit, simply send to this contract, all funds will be part of the pool
-
-    function withdraw(address token_address, address target, uint256 amount) public returns(bool){
-        require(msg.sender == erc20_bridge_address, string(abi.encodePacked("bad sender: ",toAsciiString(msg.sender))));
+    function withdraw(address token_address, address target, uint256 amount) public returns(bool) {
+        require(msg.sender == erc20_bridge_address, "msg.sender not authorized bridge");
         require(IERC20(token_address).transfer(target, amount), "token transfer failed");
         return true;
     }
-
-
-
-
-
-    function toAsciiString(address x) public returns (string memory) {
-        bytes memory s = new bytes(40);
-        for (uint i = 0; i < 20; i++) {
-            byte b = byte(uint8(uint(x) / (2**(8*(19 - i)))));
-            byte hi = byte(uint8(b) / 16);
-            byte lo = byte(uint8(b) - 16 * uint8(hi));
-            s[2*i] = char(hi);
-            s[2*i+1] = char(lo);
-        }
-        return string(s);
-    }
-
-    function char(byte b) public returns (byte c) {
-        if (uint8(b) < 10) return byte(uint8(b) + 0x30);
-        else return byte(uint8(b) + 0x57);
-    }
 }
+
+/*MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM...............MMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM...............MMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMM....................MMMMMNNMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMM....................MMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMM88=........................+MMMMMMMMMM
+MMMMMMMMMMMMMMMMM....................MMMMM...MMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMM....................MMMMM...MMMMMMMMMMMMMMM
+MMMMMMMMMMMM.........................MM+..MMM....+MMMMMMMMMM
+MMMMMMMMMNMM...................... ..MM?..MMM.. .+MMMMMMMMMM
+MMMMNDDMM+........................+MM........MM..+MMMMMMMMMM
+MMMMZ.............................+MM....................MMM
+MMMMZ.............................+MM....................MMM
+MMMMZ.............................+MM....................DDD
+MMMMZ.............................+MM..ZMMMMMMMMMMMMMMMMMMMM
+MMMMZ.............................+MM..ZMMMMMMMMMMMMMMMMMMMM
+MM..............................MMZ....ZMMMMMMMMMMMMMMMMMMMM
+MM............................MM.......ZMMMMMMMMMMMMMMMMMMMM
+MM............................MM.......ZMMMMMMMMMMMMMMMMMMMM
+MM......................ZMMMMM.......MMMMMMMMMMMMMMMMMMMMMMM
+MM............... ......ZMMMMM.... ..MMMMMMMMMMMMMMMMMMMMMMM
+MM...............MMMMM88~.........+MM..ZMMMMMMMMMMMMMMMMMMMM
+MM.......$DDDDDDD.......$DDDDD..DDNMM..ZMMMMMMMMMMMMMMMMMMMM
+MM.......$DDDDDDD.......$DDDDD..DDNMM..ZMMMMMMMMMMMMMMMMMMMM
+MM.......ZMMMMMMM.......ZMMMMM..MMMMM..ZMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMM+.......MMMMM88NMMMMM..MMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMM+.......MMMMM88NMMMMM..MMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM*/
--- ropsten_deploy_details/dev/IMultisigControl.sol
+++ ropsten_deploy_details/dev/IMultisigControl.sol
@@ -1,7 +1,8 @@
-pragma solidity ^0.5.0;
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.7.6;
 
 
-contract IMultisigControl {
+abstract contract IMultisigControl {
 
     /***************************EVENTS****************************/
     event SignerAdded(address new_signer);
@@ -11,13 +12,13 @@ contract IMultisigControl {
     //Sets threshold of signatures that must be met before function is executed. Emits 'ThresholdSet' event
     //Ethereum has no decimals, threshold is % * 10 so 50% == 500 100% == 1000
     // signatures are OK if they are >= threshold count of total valid signers
-    function set_threshold(uint16 new_threshold, uint nonce, bytes memory signatures) public;
+    function set_threshold(uint16 new_threshold, uint nonce, bytes memory signatures) public virtual;
 
     //Adds new valid signer and adjusts signer count. Emits 'SignerAdded' event
-    function add_signer(address new_signer, uint nonce, bytes memory signatures) public;
+    function add_signer(address new_signer, uint nonce, bytes memory signatures) public virtual;
 
     //Removes currently valid signer and adjust signer count. Emits 'SignerRemoved' event
-    function remove_signer(address old_signer, uint nonce, bytes memory signatures) public;
+    function remove_signer(address old_signer, uint nonce, bytes memory signatures) public virtual;
 
     //Verifies a signature bundle and returns true only if the threshold of valid signers is met,
     //this is a function that any function controlled by Vega MUST call to be securely controlled by the Vega network
@@ -26,21 +27,18 @@ contract IMultisigControl {
     // Note that validating_contract_or_submitter_address is the the submitting party. If on MultisigControl contract itself, it's the submitting ETH address
     // if function on bridge that then calls Multisig, then it's the address of that contract
     // Note also the embedded encoding, this is required to verify what function/contract the function call goes to
-    function verify_signatures(bytes memory signatures, bytes memory message, uint nonce) public returns(bool);
-
-
+    function verify_signatures(bytes memory signatures, bytes memory message, uint nonce) public virtual returns(bool);
 
     /**********************VIEWS*********************/
     //Returns number of valid signers
-    function get_valid_signer_count() public view returns(uint8);
-    
-    //Returns current threshold
-    function get_current_threshold() public view returns(uint16);
-    
-    //Returns true if address provided is valid signer
-    function is_valid_signer(address signer_address) public view returns(bool);
-    
-    //returns true if nonce has been used
-    function is_nonce_used(uint nonce) public view returns(bool);
-}
+    function get_valid_signer_count() public virtual view returns(uint8);
 
+    //Returns current threshold
+    function get_current_threshold() public virtual view returns(uint16);
+
+    //Returns true if address provided is valid signer
+    function is_valid_signer(address signer_address) public virtual view returns(bool);
+
+    //returns true if nonce has been used
+    function is_nonce_used(uint nonce) public virtual view returns(bool);
+}
