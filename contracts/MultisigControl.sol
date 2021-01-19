bdc48abaab3d41e580fcc9a0ab09b50aa6a2900b
danny <hollandcodeandgraphics@gmail.com>
2021-01-18 17:31:57 -0800

    added NatSpec formatted comments to all contracts

--- contracts/MultisigControl.sol
+++ contracts/MultisigControl.sol
@@ -22,19 +22,29 @@ contract MultisigControl is IMultisigControl {
     event SignerRemoved(address old_signer, uint256 nonce);
     event ThresholdSet(uint16 new_threshold, uint256 nonce);
 
-    //Sets threshold of signatures that must be met before function is executed. Emits 'ThresholdSet' event
-    //Ethereum has no decimals, threshold is % * 10 so 50% == 500 100% == 1000
-    // signatures are OK if they are >= threshold count of total valid signers
+    /**************************FUNCTIONS*********************/
+    /// @notice Sets threshold of signatures that must be met before function is executed.
+    /// @param new_threshold New threshold value
+    /// @param nonce Vega-assigned single-use number that provides replay attack protection
+    /// @param signatures Vega-supplied signature bundle of a validator-signed order
+    /// @notice See MultisigControl for more about signatures
+    /// @notice Ethereum has no decimals, threshold is % * 10 so 50% == 500 100% == 1000
+    /// @notice signatures are OK if they are >= threshold count of total valid signers
+    /// @dev Emits ThresholdSet event
     function set_threshold(uint16 new_threshold, uint256 nonce, bytes memory signatures) public override{
         require(new_threshold <= 1000 && new_threshold > 0, "new threshold outside range");
-
         bytes memory message = abi.encode(new_threshold, nonce, "set_threshold");
         require(verify_signatures(signatures, message, nonce), "bad signatures");
         threshold = new_threshold;
         emit ThresholdSet(new_threshold, nonce);
     }
 
-    //Adds new valid signer and adjusts signer count. Emits 'SignerAdded' event
+    /// @notice Adds new valid signer and adjusts signer count.
+    /// @param new_signer New signer address
+    /// @param nonce Vega-assigned single-use number that provides replay attack protection
+    /// @param signatures Vega-supplied signature bundle of a validator-signed order
+    /// @notice See MultisigControl for more about signatures
+    /// @dev Emits 'SignerAdded' event
     function add_signer(address new_signer, uint256 nonce, bytes memory signatures) public override{
         bytes memory message = abi.encode(new_signer, nonce, "add_signer");
         require(!signers[new_signer], "signer already exists");
@@ -44,7 +54,12 @@ contract MultisigControl is IMultisigControl {
         emit SignerAdded(new_signer, nonce);
     }
 
-    //Removes currently valid signer and adjust signer count. Emits 'SignerRemoved' event
+    /// @notice Removes currently valid signer and adjusts signer count.
+    /// @param old_signer Address of signer to be removed.
+    /// @param nonce Vega-assigned single-use number that provides replay attack protection
+    /// @param signatures Vega-supplied signature bundle of a validator-signed order
+    /// @notice See MultisigControl for more about signatures
+    /// @dev Emits 'SignerRemoved' event
     function remove_signer(address old_signer, uint256 nonce, bytes memory signatures) public override {
         bytes memory message = abi.encode(old_signer, nonce, "remove_signer");
         require(signers[old_signer], "signer doesn't exist");
@@ -54,13 +69,14 @@ contract MultisigControl is IMultisigControl {
         emit SignerRemoved(old_signer, nonce);
     }
 
-    //Verifies a signature bundle and returns true only if the threshold of valid signers is met,
-    //this is a function that any function controlled by Vega MUST call to be securely controlled by the Vega network
-    // message to hash to sign follows this pattern:
-    // abi.encode( abi.encode(param1, param2, param3, ... , nonce, function_name_string), validating_contract_or_submitter_address);
-    // Note that validating_contract_or_submitter_address is the the submitting party. If on MultisigControl contract itself, it's the submitting ETH address
-    // if function on bridge that then calls Multisig, then it's the address of that contract
-    // Note also the embedded encoding, this is required to verify what function/contract the function call goes to
+    /// @notice Verifies a signature bundle and returns true only if the threshold of valid signers is met,
+    /// @notice this is a function that any function controlled by Vega MUST call to be securely controlled by the Vega network
+    /// @notice message to hash to sign follows this pattern:
+    /// @notice abi.encode( abi.encode(param1, param2, param3, ... , nonce, function_name_string), validating_contract_or_submitter_address);
+    /// @notice Note that validating_contract_or_submitter_address is the the submitting party. If on MultisigControl contract itself, it's the submitting ETH address
+    /// @notice if function on bridge that then calls Multisig, then it's the address of that contract
+    /// @notice Note also the embedded encoding, this is required to verify what function/contract the function call goes to
+    /// @return Returns true if valid signatures are over the threshold
     function verify_signatures(bytes memory signatures, bytes memory message, uint256 nonce) public override returns(bool) {
         require(signatures.length % 65 == 0, "bad sig length");
         require(!used_nonces[nonce], "nonce already used");
@@ -75,7 +91,6 @@ contract MultisigControl is IMultisigControl {
             uint8 v;
 
             assembly {
-
             // first 32 bytes, after the length prefix
                 r := mload(add(signatures, msg_idx))
             // second 32 bytes
@@ -95,32 +110,36 @@ contract MultisigControl is IMultisigControl {
         used_nonces[nonce] = true;
         return ((uint256(sig_count) * 1000) / (uint256(signer_count))) > threshold;
     }
-    //Returns number of valid signers
+
+    /// @return Number of valid signers
     function get_valid_signer_count() public override view returns(uint8){
         return signer_count;
     }
-    event x(address addr);
-    //Returns current threshold
+
+    /// @return Current threshold
     function get_current_threshold() public override view returns(uint16) {
         return threshold;
     }
 
-    //Returns true if address provided is valid signer
+    /// @param signer_address target potential signer address
+    /// @return true if address provided is valid signer
     function is_valid_signer(address signer_address) public override view returns(bool){
         return signers[signer_address];
     }
 
-    //returns true if nonce has been used
+    /// @param nonce Nonce to lookup
+    /// @return true if nonce has been used
     function is_nonce_used(uint256 nonce) public override view returns(bool){
         return used_nonces[nonce];
     }
 }
 
-/*MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+/**
 MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM...............MMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM...............MMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMM....................MMMMMNNMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMWEMMMMMMMMMMMMMMMMMMMMMMMMMM...............MMMMMMMMMMMMM
+MMMMMMLOVEMMMMMMMMMMMMMMMMMMMMMM...............MMMMMMMMMMMMM
+MMMMMMMMMMHIXELMMMMMMMMMMMM....................MMMMMNNMMMMMM
 MMMMMMMMMMMMMMMMMMMMMMMMMMM....................MMMMMMMMMMMMM
 MMMMMMMMMMMMMMMMMMMMMM88=........................+MMMMMMMMMM
 MMMMMMMMMMMMMMMMM....................MMMMM...MMMMMMMMMMMMMMM
