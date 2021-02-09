6a18df5b750eedbb3e428afe9188a33f2687d673
danny <hollandcodeandgraphics@gmail.com>
2021-02-09 10:49:00 -0700

    deployed to stag and test, named the branch wrong. Ready for merge

--- ropsten_deploy_details/test/MultisigControl.sol
+++ ropsten_deploy_details/test/MultisigControl.sol
@@ -1,121 +1,96 @@
-pragma solidity ^0.5.0;
+//SPDX-License-Identifier: MIT
+pragma solidity 0.7.6;
 
-import "./Ownable.sol";
-import "./Killable.sol";
+import "./IMultisigControl.sol";
+contract MultisigControl is IMultisigControl {
+    constructor () {
 
-contract MultisigControl is Ownable, Killable {
-    constructor () public{
+        // set initial threshold to 50%
         threshold = 500;
-        add_signer_admin(msg.sender);
+        signers[msg.sender] = true;
+        signer_count++;
+        emit SignerAdded(msg.sender, 0);
     }
 
     uint16 threshold;
     uint8 signer_count;
     mapping(address => bool) signers;
     mapping(uint => bool) used_nonces;
+    mapping(bytes32 => mapping(address => bool)) has_signed;
 
     event SignerAdded(address new_signer, uint256 nonce);
     event SignerRemoved(address old_signer, uint256 nonce);
     event ThresholdSet(uint16 new_threshold, uint256 nonce);
 
-    /************************ SANDBOX *****/
-    function get_msg_bytes(address target) public view returns (bytes memory) {
-        //0xb89a165ea8b619c14312db316baaa80d2a98b493
-        bytes memory message = abi.encode(target, uint256(0), "add_signer");
-        return abi.encode(message, msg.sender);
-    }
-    function get_msg_hash(address target) public view returns (bytes32) {
-        return keccak256(get_msg_bytes(target));
-    }
-
-    /*************************/
-
-
-    /*******************ADMIN(note: this only is needed in order to simplify initial onboarding of nodes, once ownership is surrendered these will no longer be available)*/
-    //REMOVE BEFORE FLIGHT
-    event SignerAdded_Admin(address new_signer);
-    event SignerRemoved_Admin(address old_signer);
-    event ThresholdSet_Admin(uint16 new_threshold);
-
-    function add_signer_admin(address new_signer) public onlyOwner{
-        require(!signers[new_signer], "signer already exists");
-        signers[new_signer] = true;
-        signer_count++;
-        emit SignerAdded_Admin(new_signer);
-    }
-
-    function remove_signer_admin(address old_signer) public onlyOwner{
-        require(signers[old_signer], "signer doesn't exist");
-        signers[old_signer] = false;
-        signer_count--;
-        emit SignerRemoved_Admin(old_signer);
-    }
-
-    function set_threshold_admin(uint16 new_threshold) public onlyOwner{
-        threshold = new_threshold;
-        emit ThresholdSet_Admin(new_threshold);
-    }
-
-    /*********************END ADMIN*/
-
-
-    //Sets threshold of signatures that must be met before function is executed. Emits 'ThresholdSet' event
-    //Ethereum has no decimals, threshold is % * 10 so 50% == 500 100% == 1000
-    // signatures are OK if they are >= threshold count of total valid signers
-    function set_threshold(uint16 new_threshold, uint nonce, bytes memory signatures) public{
+    /**************************FUNCTIONS*********************/
+    /// @notice Sets threshold of signatures that must be met before function is executed.
+    /// @param new_threshold New threshold value
+    /// @param nonce Vega-assigned single-use number that provides replay attack protection
+    /// @param signatures Vega-supplied signature bundle of a validator-signed order
+    /// @notice See MultisigControl for more about signatures
+    /// @notice Ethereum has no decimals, threshold is % * 10 so 50% == 500 100% == 1000
+    /// @notice signatures are OK if they are >= threshold count of total valid signers
+    /// @dev Emits ThresholdSet event
+    function set_threshold(uint16 new_threshold, uint256 nonce, bytes memory signatures) public override{
+        require(new_threshold <= 1000 && new_threshold > 0, "new threshold outside range");
         bytes memory message = abi.encode(new_threshold, nonce, "set_threshold");
         require(verify_signatures(signatures, message, nonce), "bad signatures");
-        //todo: add sanity check
         threshold = new_threshold;
         emit ThresholdSet(new_threshold, nonce);
     }
 
-    //Adds new valid signer and adjusts signer count. Emits 'SignerAdded' event
-    function add_signer(address new_signer, uint nonce, bytes memory signatures) public {
+    /// @notice Adds new valid signer and adjusts signer count.
+    /// @param new_signer New signer address
+    /// @param nonce Vega-assigned single-use number that provides replay attack protection
+    /// @param signatures Vega-supplied signature bundle of a validator-signed order
+    /// @notice See MultisigControl for more about signatures
+    /// @dev Emits 'SignerAdded' event
+    function add_signer(address new_signer, uint256 nonce, bytes memory signatures) public override{
         bytes memory message = abi.encode(new_signer, nonce, "add_signer");
         require(!signers[new_signer], "signer already exists");
         require(verify_signatures(signatures, message, nonce), "bad signatures");
-        //todo: add sanity check
         signers[new_signer] = true;
         signer_count++;
         emit SignerAdded(new_signer, nonce);
     }
 
-    //Removes currently valid signer and adjust signer count. Emits 'SignerRemoved' event
-    function remove_signer(address old_signer, uint nonce, bytes memory signatures) public {
+    /// @notice Removes currently valid signer and adjusts signer count.
+    /// @param old_signer Address of signer to be removed.
+    /// @param nonce Vega-assigned single-use number that provides replay attack protection
+    /// @param signatures Vega-supplied signature bundle of a validator-signed order
+    /// @notice See MultisigControl for more about signatures
+    /// @dev Emits 'SignerRemoved' event
+    function remove_signer(address old_signer, uint256 nonce, bytes memory signatures) public override {
         bytes memory message = abi.encode(old_signer, nonce, "remove_signer");
         require(signers[old_signer], "signer doesn't exist");
         require(verify_signatures(signatures, message, nonce), "bad signatures");
-        //todo: add sanity check
         signers[old_signer] = false;
         signer_count--;
         emit SignerRemoved(old_signer, nonce);
     }
 
-    mapping(bytes32 => mapping(address => bool)) has_signed;
-
-    //Verifies a signature bundle and returns true only if the threshold of valid signers is met,
-    //this is a function that any function controlled by Vega MUST call to be securely controlled by the Vega network
-    // message to hash to sign follows this pattern:
-    // abi.encode( abi.encode(param1, param2, param3, ... , nonce, function_name_string), validating_contract_or_submitter_address);
-    // Note that validating_contract_or_submitter_address is the the submitting party. If on MultisigControl contract itself, it's the submitting ETH address
-    // if function on bridge that then calls Multisig, then it's the address of that contract
-    // Note also the embedded encoding, this is required to verify what function/contract the function call goes to
-    function verify_signatures(bytes memory signatures, bytes memory message, uint nonce) public returns(bool) {
+    /// @notice Verifies a signature bundle and returns true only if the threshold of valid signers is met,
+    /// @notice this is a function that any function controlled by Vega MUST call to be securely controlled by the Vega network
+    /// @notice message to hash to sign follows this pattern:
+    /// @notice abi.encode( abi.encode(param1, param2, param3, ... , nonce, function_name_string), validating_contract_or_submitter_address);
+    /// @notice Note that validating_contract_or_submitter_address is the the submitting party. If on MultisigControl contract itself, it's the submitting ETH address
+    /// @notice if function on bridge that then calls Multisig, then it's the address of that contract
+    /// @notice Note also the embedded encoding, this is required to verify what function/contract the function call goes to
+    /// @return Returns true if valid signatures are over the threshold
+    function verify_signatures(bytes memory signatures, bytes memory message, uint256 nonce) public override returns(bool) {
         require(signatures.length % 65 == 0, "bad sig length");
         require(!used_nonces[nonce], "nonce already used");
         uint8 sig_count = 0;
 
         bytes32 message_hash = keccak256(abi.encode(message, msg.sender));
 
-        for(uint msg_idx = 32; msg_idx < signatures.length + 32; msg_idx+= 65){
+        for(uint256 msg_idx = 32; msg_idx < signatures.length + 32; msg_idx+= 65){
             //recover address from that msg
             bytes32 r;
             bytes32 s;
             uint8 v;
 
             assembly {
-
             // first 32 bytes, after the length prefix
                 r := mload(add(signatures, msg_idx))
             // second 32 bytes
@@ -126,119 +101,67 @@ contract MultisigControl is Ownable, Killable {
             if (v < 27) v += 27;
 
             address recovered_address = ecrecover(message_hash, v, r, s);
+
             if(signers[recovered_address] && !has_signed[message_hash][recovered_address]){
                 has_signed[message_hash][recovered_address] = true;
                 sig_count++;
             }
         }
         used_nonces[nonce] = true;
-        //TODO: get math correct
         return ((uint256(sig_count) * 1000) / (uint256(signer_count))) > threshold;
     }
 
-    //Returns number of valid signers
-    function get_valid_signer_count() public view returns(uint8){
+    /// @return Number of valid signers
+    function get_valid_signer_count() public override view returns(uint8){
         return signer_count;
     }
 
-    //Returns current threshold
-    function get_current_threshold() public view returns(uint16) {
+    /// @return Current threshold
+    function get_current_threshold() public override view returns(uint16) {
         return threshold;
     }
 
-    //Returns true if address provided is valid signer
-    function is_valid_signer(address signer_address) public view returns(bool){
+    /// @param signer_address target potential signer address
+    /// @return true if address provided is valid signer
+    function is_valid_signer(address signer_address) public override view returns(bool){
         return signers[signer_address];
     }
 
-    //returns true if nonce has been used
-    function is_nonce_used(uint nonce) public view returns(bool){
+    /// @param nonce Nonce to lookup
+    /// @return true if nonce has been used
+    function is_nonce_used(uint256 nonce) public override view returns(bool){
         return used_nonces[nonce];
     }
 }
 
-
-
-
-/*
-function submit_signature_bundle(bytes memory signatures, bytes32 message_hash) public returns(address){
-        require(signatures.length % 65 == 0, "bad sig length");
-
-        address last_address;
-
-        for(uint msg_idx = 32; msg_idx < signatures.length + 32; msg_idx+= 65){
-            //recover address from that msg
-            bytes32 r;
-            bytes32 s;
-            uint8 v;
-
-
-            assembly {
-
-            // first 32 bytes, after the length prefix
-                r := mload(add(signatures, msg_idx))
-            // second 32 bytes
-                s := mload(add(signatures, add(msg_idx, 32)))
-            // final byte (first byte of the next 32 bytes)
-                v := byte(0, mload(add(signatures, add(msg_idx, 64))))
-            }
-            if (v < 27) v += 27;
-
-            address recovered_address = ecrecover(batch_id, v, r, s);
-            last_address = recovered_address;
-            //require(recovered_address != address(0), "cannot recover address");
-        }
-        return last_address;
-    }
-
-    function splitSignature(bytes memory sig)
-        internal
-        pure
-        returns (uint8, bytes32, bytes32)
-    {
-        require(sig.length == 65);
-
-        bytes32 r;
-        bytes32 s;
-        uint8 v;
-
-        assembly {
-            // first 32 bytes, after the length prefix
-            r := mload(add(sig, 32))
-            // second 32 bytes
-            s := mload(add(sig, 64))
-            // final byte (first byte of the next 32 bytes)
-            v := byte(0, mload(add(sig, 96)))
-        }
-
-        return (v, r, s);
-    }
-    function recoverSigner(bytes32 message, bytes memory sig)
-        public
-        pure
-        returns (address)
-    {
-        uint8 v;
-        bytes32 r;
-        bytes32 s;
-
-        (v, r, s) = splitSignature(sig);
-
-        return ecrecover(message, v, r, s);
-    }
-    address the_address = 0x80C67eEC6f8518B5Bb707ECc718B53782AC71543;
-    bool flag;
-    function recoverSignerWithFlag(bytes32 message, bytes memory sig) public returns(bool) {
-
-        flag =  recoverSigner(message, sig) == the_address;
-        return flag;
-    }
-
-    function recoverBundleWithFlag(bytes memory signatures, bytes32 batch_id)  public returns(bool) {
-
-        flag =  submit_signature_bundle(signatures, batch_id) == the_address;
-        return flag;
-    }
-
-
-*/
+/**
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMWEMMMMMMMMMMMMMMMMMMMMMMMMMM...............MMMMMMMMMMMMM
+MMMMMMLOVEMMMMMMMMMMMMMMMMMMMMMM...............MMMMMMMMMMMMM
+MMMMMMMMMMHIXELMMMMMMMMMMMM....................MMMMMNNMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMM....................MMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMM88=........................+MMMMMMMMMM
+MMMMMMMMMMMMMMMMM....................MMMMM...MMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMM....................MMMMM...MMMMMMMMMMMMMMM
+MMMMMMMMMMMM.........................MM+..MMM....+MMMMMMMMMM
+MMMMMMMMMNMM...................... ..MM?..MMM.. .+MMMMMMMMMM
+MMMMNDDMM+........................+MM........MM..+MMMMMMMMMM
+MMMMZ.............................+MM....................MMM
+MMMMZ.............................+MM....................MMM
+MMMMZ.............................+MM....................DDD
+MMMMZ.............................+MM..ZMMMMMMMMMMMMMMMMMMMM
+MMMMZ.............................+MM..ZMMMMMMMMMMMMMMMMMMMM
+MM..............................MMZ....ZMMMMMMMMMMMMMMMMMMMM
+MM............................MM.......ZMMMMMMMMMMMMMMMMMMMM
+MM............................MM.......ZMMMMMMMMMMMMMMMMMMMM
+MM......................ZMMMMM.......MMMMMMMMMMMMMMMMMMMMMMM
+MM............... ......ZMMMMM.... ..MMMMMMMMMMMMMMMMMMMMMMM
+MM...............MMMMM88~.........+MM..ZMMMMMMMMMMMMMMMMMMMM
+MM.......$DDDDDDD.......$DDDDD..DDNMM..ZMMMMMMMMMMMMMMMMMMMM
+MM.......$DDDDDDD.......$DDDDD..DDNMM..ZMMMMMMMMMMMMMMMMMMMM
+MM.......ZMMMMMMM.......ZMMMMM..MMMMM..ZMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMM+.......MMMMM88NMMMMM..MMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMM+.......MMMMM88NMMMMM..MMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM*/
