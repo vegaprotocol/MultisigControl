26ad899c20b62734ed522c44772b929cedbc4272
danny <hollandcodeandgraphics@gmail.com>
2021-02-09 10:49:00 -0700

    deployed to stag and test, named the branch wrong. Ready for merge

--- ropsten_deploy_details/stag/ERC20_Bridge_Logic.sol
+++ ropsten_deploy_details/stag/ERC20_Bridge_Logic.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.5.0;
+//SPDX-License-Identifier: MIT
+pragma solidity 0.7.6;
 
 import "./SafeMath.sol";
 import "./IERC20.sol";
@@ -6,119 +7,198 @@ import "./IERC20_Bridge_Logic.sol";
 import "./MultisigControl.sol";
 import "./ERC20_Asset_Pool.sol";
 
-//TODO remove before mainnet
-import "./Ownable.sol";
-import "./Killable.sol";
+/// @title ERC20 Bridge Logic
+/// @author Vega Protocol
+/// @notice This contract is used by Vega network users to deposit and withdraw ERC20 tokens to/from Vega.
+// @notice All funds deposited/withdrawn are to/from the assigned ERC20_Asset_Pool
+contract ERC20_Bridge_Logic is IERC20_Bridge_Logic {
 
-contract ERC20_Bridge_Logic is IERC20_Bridge_Logic, Ownable, Killable /*TODO Remove Owner and Killable before Mainnet*/ {
     //stops overflow
     using SafeMath for uint256;
 
     address multisig_control_address;
     address erc20_asset_pool_address;
+    // asset address => is listed
     mapping(address => bool) listed_tokens;
+    // asset address => minimum deposit amt
     mapping(address => uint256) minimum_deposits;
+    // asset address => maximum deposit amt
+    mapping(address => uint256) maximum_deposits;
+    // vega asset id => asset_source
+    mapping(bytes32 => address) vega_asset_ids_to_source;
+    // asset_source => vega asset id
+    mapping(address => bytes32) asset_source_to_vega_asset_id;
 
-    constructor(address erc20_asset_pool, address multisig_control) public {
+    /// @param erc20_asset_pool Initial Asset Pool contract address
+    /// @param multisig_control Initial MultisigControl contract address
+    constructor(address erc20_asset_pool, address multisig_control) {
         erc20_asset_pool_address = erc20_asset_pool;
         multisig_control_address = multisig_control;
     }
 
-    /**********************ADMIN REMOVE BEFORE MAINNET*******************************/
-    function list_asset_admin(address asset_source, uint256 asset_id, bytes32 vega_id) public onlyOwner {
-        require(asset_id == 0, "only root asset (0) allowed for ERC20");
+    /***************************FUNCTIONS*************************/
+    /// @notice This function lists the given ERC20 token contract as valid for deposit to this bridge
+    /// @param asset_source Contract address for given ERC20 token
+    /// @param vega_asset_id Vega-generated asset ID for internal use in Vega Core
+    /// @param nonce Vega-assigned single-use number that provides replay attack protection
+    /// @param signatures Vega-supplied signature bundle of a validator-signed order
+    /// @notice See MultisigControl for more about signatures
+    /// @dev Emits Asset_Listed if successful
+    function list_asset(address asset_source, bytes32 vega_asset_id, uint256 nonce, bytes memory signatures) public override {
         require(!listed_tokens[asset_source], "asset already listed");
-        listed_tokens[asset_source] = true;
-        vega_ids_to_source[vega_id] = asset_source;
-        asset_source_to_vega_id[asset_source] = vega_id;
-        emit Asset_Listed(asset_source, 0, vega_id,  uint256(asset_source) + block.number);
-    }
-    function remove_asset_admin(address asset_source, uint256 asset_id) public onlyOwner {
-        require(asset_id == 0, "only root asset (0) allowed for ERC20");
-        require(listed_tokens[asset_source], "asset not listed");
-        listed_tokens[asset_source] = false;
-        emit Asset_Removed(asset_source, 0,  uint256(asset_source) + block.number);
-    }
-    /**********************END ADMIN*******************************/
-
-    //vega_id => asset_source
-    mapping(bytes32 => address) vega_ids_to_source;
-    mapping(address => bytes32) asset_source_to_vega_id;
-
-    function get_vega_id(address asset_source, uint256 asset_id) public view returns(bytes32){
-        require(asset_id == 0, "only root asset (0) allowed for ERC20");
-        return asset_source_to_vega_id[asset_source];
-    }
-    function get_asset_source_and_asset_id(bytes32 vega_id) public view returns(address, uint256){
-        return (vega_ids_to_source[vega_id], 0);
-    }
-
-    function list_asset(address asset_source, uint256 asset_id, bytes32 vega_id, uint256 nonce, bytes memory signatures) public {
-        require(asset_id == 0, "only root asset (0) allowed for ERC20");
-        require(!listed_tokens[asset_source], "asset already listed");
-        bytes memory message = abi.encode(asset_source, asset_id, vega_id, nonce, 'list_asset');
-        require(MultisigControl(multisig_control_address).verify_signatures(signatures, message, nonce));
-        listed_tokens[asset_source] = true;
-        vega_ids_to_source[vega_id] = asset_source;
-        asset_source_to_vega_id[asset_source] = vega_id;
-        emit Asset_Listed(asset_source, 0, vega_id, nonce);
-    }
-    function remove_asset(address asset_source, uint256 asset_id, uint256 nonce, bytes memory signatures) public {
-        require(asset_id == 0, "only root asset (0) allowed for ERC20");
-        require(listed_tokens[asset_source], "asset not listed");
-        bytes memory message = abi.encode(asset_source, asset_id, nonce, 'remove_asset');
-        require(MultisigControl(multisig_control_address).verify_signatures(signatures, message, nonce));
-        listed_tokens[asset_source] = false;
-        emit Asset_Removed(asset_source, 0, nonce);
-    }
-    function set_deposit_minimum(address asset_source, uint256 asset_id, uint256 nonce, uint256 minimum_amount, bytes memory signatures) public{
-        require(asset_id == 0, "only root asset (0) allowed for ERC20");
-        require(listed_tokens[asset_source], "asset not listed");
-
-        bytes memory message = abi.encode(asset_source, asset_id, minimum_amount, nonce, 'set_deposit_minimum');
-        require(MultisigControl(multisig_control_address).verify_signatures(signatures, message, nonce));
-        minimum_deposits[asset_source] = minimum_amount;
-        emit Asset_Deposit_Minimum_Set(asset_source, 0, minimum_amount, nonce);
-
-    }
-    function withdraw_asset(address asset_source, uint256 asset_id, uint256 amount, uint256 expiry, uint256 nonce, bytes memory signatures) public {
-        require(asset_id == 0, "only root asset (0) allowed for ERC20");
-        require(listed_tokens[asset_source], "asset not listed");
-        require(expiry > block.timestamp, "withdrawal has expired");
-
-        bytes memory message = abi.encode(asset_source, asset_id, amount, expiry, msg.sender,  nonce, 'withdraw_asset');
+        bytes memory message = abi.encode(asset_source, vega_asset_id, nonce, 'list_asset');
         require(MultisigControl(multisig_control_address).verify_signatures(signatures, message, nonce), "bad signatures");
-
-        require(ERC20_Asset_Pool(erc20_asset_pool_address).withdraw(asset_source, msg.sender, amount), "token didn't transfer, rejected by asset pool.");
-
-        emit Asset_Withdrawn(msg.sender, asset_source, 0, amount, nonce);
+        listed_tokens[asset_source] = true;
+        vega_asset_ids_to_source[vega_asset_id] = asset_source;
+        asset_source_to_vega_asset_id[asset_source] = vega_asset_id;
+        emit Asset_Listed(asset_source, vega_asset_id, nonce);
     }
-    function deposit_asset(address asset_source, uint256 asset_id, uint256 amount, bytes32 vega_public_key) public {
-        require(asset_id == 0, "only root asset (0) allowed for ERC20");
+
+    /// @notice This function removes from listing the given ERC20 token contract. This marks the token as valid for deposit to this bridge
+    /// @param asset_source Contract address for given ERC20 token
+    /// @param nonce Vega-assigned single-use number that provides replay attack protection
+    /// @param signatures Vega-supplied signature bundle of a validator-signed order
+    /// @notice See MultisigControl for more about signatures
+    /// @dev Emits Asset_Removed if successful
+    function remove_asset(address asset_source, uint256 nonce, bytes memory signatures) public override {
+        require(listed_tokens[asset_source], "asset not listed");
+        bytes memory message = abi.encode(asset_source, nonce, 'remove_asset');
+        require(MultisigControl(multisig_control_address).verify_signatures(signatures, message, nonce), "bad signatures");
+        listed_tokens[asset_source] = false;
+        emit Asset_Removed(asset_source, nonce);
+    }
+
+    /// @notice This function sets the minimum allowable deposit for the given ERC20 token
+    /// @param asset_source Contract address for given ERC20 token
+    /// @param minimum_amount Minimum deposit amount
+    /// @param nonce Vega-assigned single-use number that provides replay attack protection
+    /// @param signatures Vega-supplied signature bundle of a validator-signed order
+    /// @notice See MultisigControl for more about signatures
+    /// @dev Emits Asset_Deposit_Minimum_Set if successful
+    function set_deposit_minimum(address asset_source, uint256 minimum_amount, uint256 nonce, bytes memory signatures) public override{
+        require(listed_tokens[asset_source], "asset not listed");
+        bytes memory message = abi.encode(asset_source, minimum_amount, nonce, 'set_deposit_minimum');
+        require(MultisigControl(multisig_control_address).verify_signatures(signatures, message, nonce), "bad signatures");
+        minimum_deposits[asset_source] = minimum_amount;
+        emit Asset_Deposit_Minimum_Set(asset_source, minimum_amount, nonce);
+    }
+
+    /// @notice This function sets the maximum allowable deposit for the given ERC20 token
+    /// @param asset_source Contract address for given ERC20 token
+    /// @param maximum_amount Maximum deposit amount
+    /// @param nonce Vega-assigned single-use number that provides replay attack protection
+    /// @param signatures Vega-supplied signature bundle of a validator-signed order
+    /// @notice See MultisigControl for more about signatures
+    /// @dev Emits Asset_Deposit_Maximum_Set if successful
+    function set_deposit_maximum(address asset_source, uint256 maximum_amount, uint256 nonce, bytes memory signatures) public override {
+        require(listed_tokens[asset_source], "asset not listed");
+        bytes memory message = abi.encode(asset_source, maximum_amount, nonce, 'set_deposit_maximum');
+        require(MultisigControl(multisig_control_address).verify_signatures(signatures, message, nonce), "bad signatures");
+        maximum_deposits[asset_source] = maximum_amount;
+        emit Asset_Deposit_Maximum_Set(asset_source, maximum_amount, nonce);
+    }
+
+    /// @notice This function sets the maximum allowable deposit for the given ERC20 token
+    /// @param asset_source Contract address for given ERC20 token
+    /// @param amount Amount of ERC20 tokens to withdraw
+    /// @param expiry Vega-assigned timestamp of withdrawal order expiration
+    /// @param target Target Ethereum address to receive withdrawn ERC20 tokens
+    /// @param nonce Vega-assigned single-use number that provides replay attack protection
+    /// @param signatures Vega-supplied signature bundle of a validator-signed order
+    /// @notice See MultisigControl for more about signatures
+    /// @dev Emits Asset_Withdrawn if successful
+    function withdraw_asset(address asset_source, uint256 amount, uint256 expiry, address target, uint256 nonce, bytes memory signatures) public  override{
+        require(expiry > block.timestamp, "withdrawal has expired");
+        bytes memory message = abi.encode(asset_source, amount, expiry, target,  nonce, 'withdraw_asset');
+        require(MultisigControl(multisig_control_address).verify_signatures(signatures, message, nonce), "bad signatures");
+        require(ERC20_Asset_Pool(erc20_asset_pool_address).withdraw(asset_source, target, amount), "token didn't transfer, rejected by asset pool.");
+        emit Asset_Withdrawn(target, asset_source, amount, nonce);
+    }
+
+    /// @notice This function allows a user to deposit given ERC20 tokens into Vega
+    /// @param asset_source Contract address for given ERC20 token
+    /// @param amount Amount of tokens to be deposited into Vega
+    /// @param vega_public_key Target vega public key to be credited with this deposit
+    /// @dev MUST emit Asset_Deposited if successful
+    /// @dev ERC20 approve function should be run before running this
+    /// @notice ERC20 approve function should be run before running this
+    function deposit_asset(address asset_source, uint256 amount, bytes32 vega_public_key) public override {
         require(listed_tokens[asset_source], "asset not listed");
         //User must run approve before deposit
-
+        require(maximum_deposits[asset_source] == 0 || amount <= maximum_deposits[asset_source], "deposit above maximum");
         require(amount >= minimum_deposits[asset_source], "deposit below minimum");
         require(IERC20(asset_source).transferFrom(msg.sender, erc20_asset_pool_address, amount), "transfer failed in deposit");
-
-        emit Asset_Deposited(msg.sender, asset_source, 0, amount, vega_public_key);
+        emit Asset_Deposited(msg.sender, asset_source, amount, vega_public_key);
     }
 
-    // VIEWS /////////////////
-    function is_asset_listed(address asset_source, uint256 asset_id) public view returns(bool){
-        return listed_tokens[asset_source] && asset_id == 0;
+    /***************************VIEWS*****************************/
+    /// @notice This view returns true if the given ERC20 token contract has been listed valid for deposit
+    /// @param asset_source Contract address for given ERC20 token
+    /// @return True if asset is listed
+    function is_asset_listed(address asset_source) public override view returns(bool){
+        return listed_tokens[asset_source];
     }
-    function get_deposit_minimum(address asset_source, uint256 asset_id) public view returns(uint256){
-        if(asset_id != 0){
-            return 0;
-        } else {
-            return minimum_deposits[asset_source];
-        }
+
+    /// @notice This view returns minimum valid deposit
+    /// @param asset_source Contract address for given ERC20 token
+    /// @return Minimum valid deposit of given ERC20 token
+    function get_deposit_minimum(address asset_source) public override view returns(uint256){
+        return minimum_deposits[asset_source];
     }
-    function get_multisig_control_address() public view returns(address) {
+
+    /// @notice This view returns maximum valid deposit
+    /// @param asset_source Contract address for given ERC20 token
+    /// @return Maximum valid deposit of given ERC20 token
+    function get_deposit_maximum(address asset_source) public override view returns(uint256){
+        return maximum_deposits[asset_source];
+    }
+
+    /// @return current multisig_control_address
+    function get_multisig_control_address() public override view returns(address) {
         return multisig_control_address;
     }
 
+    /// @param asset_source Contract address for given ERC20 token
+    /// @return The assigned Vega Asset Id for given ERC20 token
+    function get_vega_asset_id(address asset_source) public override view returns(bytes32){
+        return asset_source_to_vega_asset_id[asset_source];
+    }
 
-
+    /// @param vega_asset_id Vega-assigned asset ID for which you want the ERC20 token address
+    /// @return The ERC20 token contract address for a given Vega Asset Id
+    function get_asset_source(bytes32 vega_asset_id) public override view returns(address){
+        return vega_asset_ids_to_source[vega_asset_id];
+    }
 }
+
+/**
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMWEMMMMMMMMMMMMMMMMMMMMMMMMMM...............MMMMMMMMMMMMM
+MMMMMMLOVEMMMMMMMMMMMMMMMMMMMMMM...............MMMMMMMMMMMMM
+MMMMMMMMMMHIXELMMMMMMMMMMMM....................MMMMMNNMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMM....................MMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMM88=........................+MMMMMMMMMM
+MMMMMMMMMMMMMMMMM....................MMMMM...MMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMM....................MMMMM...MMMMMMMMMMMMMMM
+MMMMMMMMMMMM.........................MM+..MMM....+MMMMMMMMMM
+MMMMMMMMMNMM...................... ..MM?..MMM.. .+MMMMMMMMMM
+MMMMNDDMM+........................+MM........MM..+MMMMMMMMMM
+MMMMZ.............................+MM....................MMM
+MMMMZ.............................+MM....................MMM
+MMMMZ.............................+MM....................DDD
+MMMMZ.............................+MM..ZMMMMMMMMMMMMMMMMMMMM
+MMMMZ.............................+MM..ZMMMMMMMMMMMMMMMMMMMM
+MM..............................MMZ....ZMMMMMMMMMMMMMMMMMMMM
+MM............................MM.......ZMMMMMMMMMMMMMMMMMMMM
+MM............................MM.......ZMMMMMMMMMMMMMMMMMMMM
+MM......................ZMMMMM.......MMMMMMMMMMMMMMMMMMMMMMM
+MM............... ......ZMMMMM.... ..MMMMMMMMMMMMMMMMMMMMMMM
+MM...............MMMMM88~.........+MM..ZMMMMMMMMMMMMMMMMMMMM
+MM.......$DDDDDDD.......$DDDDD..DDNMM..ZMMMMMMMMMMMMMMMMMMMM
+MM.......$DDDDDDD.......$DDDDD..DDNMM..ZMMMMMMMMMMMMMMMMMMMM
+MM.......ZMMMMMMM.......ZMMMMM..MMMMM..ZMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMM+.......MMMMM88NMMMMM..MMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMM+.......MMMMM88NMMMMM..MMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM*/
--- ropsten_deploy_details/stag/Migrations.sol
+++ ropsten_deploy_details/stag/Migrations.sol
@@ -1,16 +1,16 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.8.0;
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.7.6;
 
 contract Migrations {
-  address public owner = msg.sender;
+  address public owner;
   uint public last_completed_migration;
 
+  constructor() {
+    owner = msg.sender;
+  }
+
   modifier restricted() {
-    require(
-      msg.sender == owner,
-      "This function is restricted to the contract's owner"
-    );
-    _;
+    if (msg.sender == owner) _;
   }
 
   function setCompleted(uint completed) public restricted {
