7d1be8b2a74ee735f3a963b106ffcce3da6a84b6
Jeremy Letang <me@jeremyletang.com>
2023-06-13 14:57:26 +0100

    chore: playing around with unchecked
    Signed-off-by: Jeremy Letang <me@jeremyletang.com>

--- contracts/MultisigControl.sol
+++ contracts/MultisigControl.sol
@@ -115,7 +115,7 @@ contract MultisigControl is IMultisigControl {
         assembly {
             offset := signatures.offset
         }
-        for (uint256 msg_idx = 0; msg_idx < signatures.length; msg_idx += 65) {
+        for (uint256 msg_idx = 0; msg_idx < signatures.length;) {
             //recover address from that msg
             bytes32 r;
             bytes32 s;
@@ -141,17 +141,19 @@ contract MultisigControl is IMultisigControl {
                 uint256(s) <= 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0,
                 "Malleable signature error"
             );
-            if (v < 27) v += 27;
+	    if (v < 27) v += 27;
 
             address recovered_address = ecrecover(message_hash, v, r, s);
 
             if (signers[recovered_address] && !has_signed(signers_temp, recovered_address, size)) {
                 signers_temp[size] = recovered_address;
-                size++;
+                unchecked { size++; }
             }
-        }
 
-        used_nonces[nonce] = ((uint256(size) * 1000) / (uint256(signer_count))) > threshold;
+	    unchecked { msg_idx += 65; }
+	}
+
+        unchecked { used_nonces[nonce] = ((uint256(size) * 1000) / (uint256(signer_count))) > threshold; }
         return used_nonces[nonce];
     }
 
@@ -160,10 +162,12 @@ contract MultisigControl is IMultisigControl {
         address signer,
         uint8 size
     ) private pure returns (bool) {
-        for (uint256 i; i < size; i++) {
+        for (uint256 i; i < size;) {
             if (signers_temp[i] == signer) {
                 return true;
             }
+
+	    unchecked { i++; }
         }
         return false;
     }
